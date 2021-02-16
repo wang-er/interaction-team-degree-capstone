dd9ee0c26c93b90154e171b6d95ba55c81085285
Harumi Jang <harumijang@Harumis-MacBook-Pro.local>
2021-02-16 01:24:50 -0500

    photo upload test with bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import TestList from './components/test-list'
+import logo from "./logo.svg";
+import "./App.css";
+import TestList from "./components/test-list";
+import ImageUpload from "./components/ImageUpload/index";
 
 function App() {
   return (
     <div>
       <h1>React + Firebase</h1>
-      <TestList/>
+      <TestList />
+      <ImageUpload />
     </div>
   );
 }
